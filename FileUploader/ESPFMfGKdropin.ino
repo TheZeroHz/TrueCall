dab2e1ee567f1f9ba20b4fe0d2ee894341753942
Rakib Hasan <59503211+TheZeroHz@users.noreply.github.com>
2025-09-24 13:11:37 +0600

    Add files via upload

---
+++ FileUploader/ESPFMfGKdropin.ino
@@ -0,0 +1,55 @@
+// Adds the filé systems
+void addFileSystems(void) {
+  // This adds the Storage into the Filemanager. You have to at least call one of those.
+  // If you don't, begin() will fail. Because a Filemanager without files is useless.
+
+  /**/  //<-- Addd space there like this /** /
+  if (FFat.begin(true)) {
+    if (!filemgr.AddFS(FFat, "Flash/FFat", false)) {
+      Serial.println(F("Adding FFAT failed."));
+    }
+  } else {
+    Serial.println(F("FFat File System not inited."));
+  }
+}
+
+uint32_t checkFileFlags(fs::FS &fs, String filename, uint32_t flags) {
+  // Checks if target file name is valid for action. This will simply allow everything by returning the queried flag
+  if (flags & ESPFMfGK::flagIsValidAction) {
+    return flags & (~ESPFMfGK::flagIsValidAction);
+  }
+
+  // Checks if target file name is valid for action.
+  if (flags & ESPFMfGK::flagIsValidTargetFilename) {
+    return flags & (~ESPFMfGK::flagIsValidTargetFilename);
+  }
+
+  // Default actions
+  uint32_t defaultflags = ESPFMfGK::flagCanDelete | ESPFMfGK::flagCanRename | ESPFMfGK::flagCanGZip |  // ^t
+                          ESPFMfGK::flagCanDownload | ESPFMfGK::flagCanUpload | ESPFMfGK::flagCanEdit | // ^t
+                          ESPFMfGK::flagAllowPreview;
+
+  return defaultflags;
+}
+
+void setupFilemanager(void) {
+  // See above.
+  filemgr.checkFileFlags = checkFileFlags;
+
+  filemgr.WebPageTitle = "FileManager";
+  filemgr.BackgroundColor = "white";
+  filemgr.textareaCharset = "accept-charset=\"utf-8\"";
+
+  if ((WiFi.status() == WL_CONNECTED) && (filemgr.begin())) {
+    Serial.print(F("Open Filemanager with http://"));
+    Serial.print(WiFi.localIP());
+    Serial.print(F(":"));
+    Serial.print(filemanagerport);
+    Serial.print(F("/"));
+    Serial.println();
+  } else {
+    Serial.print(F("Filemanager: did not start"));
+  }
+}
+
+//
